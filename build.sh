fe4ce988bf76fc17e5d04e0d166771c6be79bb32
DarkVenomLS <sarodelokesh005@gmail.com>
2025-04-03 01:54:01 +0530

    build.sh Updated

--- build.sh
+++ build.sh
@@ -5,8 +5,7 @@ pip install -r requirements.txt
 
 python manage.py collectstatic --no-input
 python manage.py migrate
-if [[ "$CREATE_SUPERUSER" == "True" ]];
-then
-    python manage.py createsuperuser --no-input --email "$DJANGO_SUPERUSER_EMAIL"
+if [[ $CREATE_SUPERUSER == "True" && -n "$DJANGO_SUPERUSER_USERNAME" && -n "$DJANGO_SUPERUSER_EMAIL" && -n "$DJANGO_SUPERUSER_PASSWORD" ]]; then
+    python manage.py createsuperuser --no-input --username "$DJANGO_SUPERUSER_USERNAME" --email "$DJANGO_SUPERUSER_EMAIL" || true
 fi
 
